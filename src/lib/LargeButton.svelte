0464a243039bb32ec7b633ff459cf0d9fa5828cd
Rioni <nrosa01@ucm.es>
2022-10-19 04:00:47 +0200

    Button bug correction

--- src/lib/LargeButton.svelte
+++ src/lib/LargeButton.svelte
@@ -17,19 +17,32 @@
             return;
 
         let button = document.getElementById("button");
+        let cssButton = window.getComputedStyle(button, null);
+        let div = document.getElementById("div");
+        let cssDiv = window.getComputedStyle(div, null);
 
         currentPos = newPos;
         //newpos must bet -50 50
         newPos = Math.floor(Math.random() * 100) - 50;
 
         while (Math.abs(newPos - currentPos) < 25)
-        {
             newPos = Math.floor(Math.random() * 100) - 50;
-        }
 
         if(canBeClicked)
             newPos = 0;
 
+        let buttonWidth = parseInt(cssButton.width);     
+        let divWidth = parseInt(cssDiv.width);
+
+        //lambda function to convert pixels to percentage
+        let pixelsToPercentage = (pixels) => (pixels / divWidth) * 100;
+        let percentageToPixels = (percentage) => (percentage / 100) * divWidth;
+
+        let maxRight = pixelsToPercentage(divWidth/2  - (buttonWidth/2))
+        let maxLeft = pixelsToPercentage(-divWidth/2 + (buttonWidth/2))
+
+        //Clamp newpos between maxRight and maxLeft
+        newPos = Math.min(Math.max(newPos, maxLeft), maxRight);
         button.style.left = `${newPos}%`;
     }
 
